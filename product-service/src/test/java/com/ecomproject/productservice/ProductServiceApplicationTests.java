f054acf8f2bbb9ce5fc19e4213a151087f3d00c4
Hammad <hammadrashid98@gmail.com>
2023-01-21 17:10:43 +0300

    Added tests

--- product-service/src/test/java/com/ecomproject/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/ecomproject/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,67 @@
 package com.ecomproject.productservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.ecomproject.productservice.dto.ProductRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.assertions.Assertions;
+import com.ecomproject.productservice.repository.ProductRepository;
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
+	@Container	
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+
+
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() throws Exception 
+	{
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+		.contentType(MediaType.APPLICATION_JSON)
+		.content(productRequestString)).andExpect(status().isCreated());
+
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+	}
+
+	private ProductRequest getProductRequest(){
+		return ProductRequest.builder()
+		.name("iPhone 6")
+		.description("iPhone 13 description")
+		.price(BigDecimal.valueOf(1200)).build();
 	}
 
 }
