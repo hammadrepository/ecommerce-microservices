c482ddba9489d59cf1c9c0f7bf2e830e8f00afd6
hammadrashid <hammadrashid98@gmail.com>
2023-08-27 20:29:17 +0300

    Added Circuit Breaker

--- order-service/src/main/java/com/ecommerceproject/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/ecommerceproject/orderservice/service/OrderService.java
@@ -27,7 +27,7 @@ public class OrderService {
     private final WebClient.Builder webClientBuilder;
 
     @ExceptionHandler
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
 
         order.setOrderNumber((UUID.randomUUID().toString()));
@@ -51,6 +51,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order placed successfully";
         }else{
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
